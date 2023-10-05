78776fe940ad944b1fbb5e79685b9b3e8652d16c
MOHAMEDGAD2101 <mohammedgad2101@gmail.com>
2023-10-05 01:36:30 -0700

    my first commint0

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include <unistd.h>
 /**
 *main -entry point
 *description : print a quote us writing function
@@ -8,6 +8,7 @@
 int main(void)
 {
 	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n ";
+
 	write(1, quo, 59);
 
 	return (1);
