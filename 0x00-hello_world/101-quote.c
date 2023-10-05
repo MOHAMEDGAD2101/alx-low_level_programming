d5c3113404869136452b8b776a8663e8b2c243f7
MOHAMEDGAD2101 <mohammedgad2101@gmail.com>
2023-10-05 01:52:58 -0700

    my first commint0

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,18 @@
 #include <stdio.h>
 #include <unistd.h>
+
 /**
 * main - Entry point
+*
 * Description: print a quote us writing function
+*
 * ssize_t write(int fd.const void *buf,size_t count):
-*return : 1 (error)
+*
+* Return : Always 1 (Success)
 */
 int main(void)
 {
-fput("and that place of art is useful\" - dora kor par, 2015-10-19\n", stdout);
-return (1);
+	write(2,
+	"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
