152b484436c397615a54e72cac657fe5bddb631d
MOHAMEDGAD2101 <mohammedgad2101@gmail.com>
2023-10-06 11:31:21 -0700

    my first commint0

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -2,21 +2,18 @@
 
 /**
  * main - Entry point
- * Description: print 0-9 using putchar
- *              while using int variable
+ * Description: 'Print number of base 10 using putchar'
  *
  * Return: always 0 (Success)
  */
 
 int main(void)
 {
-		int digit = 0;
+		int n
 
-		while (digit <= 9)
+		for (n = 0; n <= 9; n++)
 		{
-			/*convert digit to ASCII representation*/
-			putchar(digit + '0');
-			digit++;
+			putchar(n +48);
 		}
 		putchar('\n');
 
