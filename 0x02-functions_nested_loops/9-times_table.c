bbeb35198c97aff3989a36b641e806393de2980f
MOHAMEDGAD2101 <mohammedgad2101@gmail.com>
2023-10-11 00:13:34 -0700

    my first commint

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -11,25 +11,24 @@ void times_table(void)
 	int i, j, k;
 
 	for (i = 0; i <= 9; i++)
-	{
+	
 		_putchar(48);
 		for (j = 1; j <= 9; j++)
 		{
 			_putchar(',');
 			_putchar(' ');
 
-			if (k = i * j);
+			k = i * j
 			/*
 			 * put space if product is a single number
 			 * place the first digit if its two number
 			 */
-			if (k <= 9);
+			if (k <= 9)
 				_putchar('');
-			if else
-
+			else
 				_putchar((k / 10) + 48); /*get the first digit*/
 
-			_putchar((prod % 10) + 48); /*get the second digit*/
+			_putchar((k % 10) + 48); /*get the second digit*/
 		}
 		_putchar('\n');
 	}
