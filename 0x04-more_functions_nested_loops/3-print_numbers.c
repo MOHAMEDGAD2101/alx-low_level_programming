38bc970d8b4b1b13e51a7e40854e895f7d0ea356
MOHAMEDGAD2101 <mohammedgad2101@gmail.com>
2023-10-12 05:47:33 -0700

    my first commint0

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -8,11 +8,11 @@
  */
 void print_numbers(void)
 {
-int num = 0;
+	int num = 0;
 
 	do {
 		_putchar(num + 48);
 		num++;
-	} while (num >= 0 && num <= 9(;
-	_putchar)'\n');
+	} while (num >= 0 && num <= 9);
+	_putchar('\n');
 }
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,9 +1,12 @@
 #ifndef main_h
 #define main_h
 
+#include <stdio.h>
+
 int _putchar(char);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
 void print_numbers(void);
+void print_most_numbers(void);
 #endif
